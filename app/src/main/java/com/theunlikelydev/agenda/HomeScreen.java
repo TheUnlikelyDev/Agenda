fe20b03e6722ab8a6ae7d9090acf621bda29c922
William Morris <morrissam@ecs.vuw.ac.nz>
2018-04-10 18:48:46 +1200

    added HomeScreen Activity declaration

---
+++ app/src/main/java/com/theunlikelydev/agenda/HomeScreen.java
@@ -0,0 +1,12 @@
+package com.theunlikelydev.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
