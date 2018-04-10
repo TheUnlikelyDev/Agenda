8f39468171925eb66a8833fc428e1be2c4e7007c
William Morris <morrissam@ecs.vuw.ac.nz>
2018-04-10 20:18:24 +1200

    added HomeScreen layout and method in HomeScreen for setting the text of agenda_title textview dynamically( getting current date ).

--- app/src/main/java/com/theunlikelydev/agenda/HomeScreen.java
+++ app/src/main/java/com/theunlikelydev/agenda/HomeScreen.java
@@ -1,12 +1,42 @@
 package com.theunlikelydev.agenda;
 
 import android.app.Activity;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HomeScreen extends Activity {
+
+    private TextView agendaTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen_act_layout);
+
+        this.agendaTitle = findViewById(R.id.daily_agenda_title);
+        setAgendaTitle();
+
+    }
+
+    private void setAgendaTitle(){
+
+        String currentDate = getDateTime();
+        String text = getResources().getString(R.string.agenda_title);
+        String title =text + " " + currentDate;
+        agendaTitle.setText(title);
+
+    }
+
+    private String getDateTime() {
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        return dateFormat.format(date);
+
     }
 }
