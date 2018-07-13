2121fd7b61f559930115a7f2a799b42bb7264075
WSM_Desktop <morrissam@ecs.vuw.ac.nz>
2018-07-13 15:17:09 +1200

    finished display for view task dialog

--- app/src/main/java/com/theunlikelydev/agenda/TaskViewDialog.java
+++ app/src/main/java/com/theunlikelydev/agenda/TaskViewDialog.java
@@ -4,9 +4,11 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -25,17 +27,31 @@ public class TaskViewDialog extends android.app.DialogFragment {
 
         TextView title = view.findViewById(R.id.title_tv);
         TextView description = view.findViewById(R.id.description_tv);
-        TextView overflow = view.findViewById(R.id.overflow_tv);
+        TextView taskTitle = view.findViewById(R.id.task_title);
+
+
+
         title.setText(taskInfo[0]);
         description.setText(taskInfo[1]);
-        overflow.setText(taskInfo[2]);
+
+        Button okBut = view.findViewById(R.id.ok);
+        okBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        Typeface face = Typeface.createFromAsset(getActivity().getAssets(),
+                "fonts/carterone.ttf");
+        okBut.setTypeface(face);
+        taskTitle.setTypeface(face);
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        return(builder.setTitle(R.string.view_dialog_text)
+        return(builder
                 .setView(view)
-                .setPositiveButton(android.R.string.ok, null)
                 .create());
     }
 
